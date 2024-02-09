7871dd35c97ea8a346f63e76f3741d068dab6a1a
SMJ426 <jerry04260@gmail.com>
2024-02-10 01:22:53 +0900

    style: 메인 스플래쉬 패이지 제작

--- src/Pages/Main/PageMain.jsx
+++ src/Pages/Main/PageMain.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const StyledMainWrapper = styled.div`
-  background-color: skyblue;
-`;
+import flameImg from '../../images/flame.gif';
 
 export default function PageMain() {
   return (
     <StyledMainWrapper>
-      <h2>메인 페이지입니다.</h2>
+      <h1 className="title">CHATTING PROJECT</h1>
+      <p>made by SMJ426 & stophyeon</p>
     </StyledMainWrapper>
   );
 }
+
+const StyledMainWrapper = styled.div`
+  h1 {
+    margin: 0;
+    font-size: 10rem;
+    text-align: center;
+  }
+
+  .title {
+    background-image: url(${flameImg});
+    background-size: cover;
+    background-position: center;
+    color: transparent;
+    background-clip: text;
+    -webkit-background-clip: text;
+  }
+`;
